5410025ab980b8748bf30104e851b0049f4bf0fe
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-10-05 12:50:02 -0700

    Fetching from Coindesk API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import Fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
 
-const Index = () => (
+const Index = props => (
   <Layout>
     <div>
       <h1>Hello Next.js</h1>
     </div>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+
+  return {
+    bpi: data.bpi
+  };
+};
 
 export default Index;
