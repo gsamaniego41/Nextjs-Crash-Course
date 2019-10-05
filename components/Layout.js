1c2d6b6ea81c083c932451b18e5307266c037047
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-10-05 12:09:04 -0700

    Next Head component

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,15 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 
 const Layout = props => (
   <div>
+    <Head>
+      <title>Bitzprice</title>
+      <link
+        rel="stylesheet"
+        href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
+      />
+    </Head>
     <Navbar />
     {props.children}
   </div>
